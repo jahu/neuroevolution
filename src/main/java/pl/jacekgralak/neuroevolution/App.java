a34e0dd265183d3483a15e8fbc3c084ff854cbdb
Jacek Gralak <jacekgralak@gmail.com>
2014-12-17 23:56:48 +0100

    Initial commit

---
+++ src/main/java/pl/jacekgralak/neuroevolution/App.java
@@ -0,0 +1,13 @@
+package pl.jacekgralak.neuroevolution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
